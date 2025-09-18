51b74015b9821efa87a64c7fcddab7ee11f73bec
MD. MAHMUDUL HASAN <i.mahmudulhasan1971@gmail.com>
2025-09-18 21:38:47 +0600

    Update Data_types.py

--- Python/Data_types.py
+++ Python/Data_types.py
@@ -4,10 +4,12 @@ print(type(Age))
 
 name = "Mahmudul Hasan"
 print(name)
+print(type(name))
 
 cgpa = 3.94
 print(cgpa)
+print(type(cgpa))
 
 Is_Completed= True
 print(Is_Completed)
-
+print(type(Is_Completed))
